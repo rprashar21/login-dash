784e93be8768fa3596b06366ff4025a489527092
Rohit Prashar <rohitprashar712@gmail.com>
2025-06-30 19:18:25 +0100

    login component refactor

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import './App.css'
 import Counter from "./practice/Counter.jsx";
 import LoginForm from "./components/LoginSignup/LoginForm.jsx";
-import {LoginForm01} from "./components/LoginSignup/LoginForm01.jsx";
+import {LoginForm} from "./components/LoginSignup/LoginForm.jsx";
 
 function App() {
 
     return (
         <>
             <div>
-                <LoginForm01/>
+                <LoginForm/>
             </div>
            {/*<Counter/>*/}
           {/*<LoginForm/>*/}
